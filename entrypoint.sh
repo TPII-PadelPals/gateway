20617b60c016d6be5b91fa3906ecad6d7f82199a
Amaru Gabriel Durán <amaruduran@gmail.com>
2025-06-02 12:20:35 -0300

    Sprint 25 (#5)
    * feat: added traefik config for jwt validation
    
    * refactor: readme
    
    * feat: add  X_API_KEY to conf
    
    ---------
    
    Co-authored-by: tdelpup <tomasgdelpup@gmail.com>

---
+++ entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -e
+
+mkdir -p /etc/traefik/dynamic
+
+cat <<EOF > /etc/traefik/dynamic/middleware.yml
+http:
+  middlewares:
+    my-easy-traefik-rate-limit-jwt:
+      plugin:
+        easy-traefik-rate-limit-jwt:
+          Alg: HS256
+          ErrorMessage: "Authentication failed."
+          ExpirationMessage: "Your session has expired. Please log in again."
+          JwtPayloadFields:
+            - exp
+            - sub
+            - iat
+          RoutesToBypassJwtValidation:
+            - match: PathPrefix(\`/api/v1/auth\`)
+          RoutesToBypassTokenExpiration:
+            - match: PathPrefix(\`/api/v1/auth\`)
+          Secret:
+            - ${JWT_SECRET}
+          Sources:
+            - key: Authorization
+              type: bearer
+          InjectNewHeaders:
+            X-User-ID:
+              From:
+                - JwtPayloadFields
+              Values:
+                - sub
+          logs:
+            level: DEBUG
+    x-api-key:
+      headers:
+        customRequestHeaders:
+          x-api-key: ${X_API_KEY}
+EOF
+
+exec traefik "$@"
