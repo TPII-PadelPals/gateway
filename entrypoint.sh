502904db0ad83169a2c6967c16207f8684f71cd1
tdelpup <tomasgdelpup@gmail.com>
2025-06-08 22:04:58 +0000

    feat: aws settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,4 +39,95 @@ http:
           x-api-key: ${X_API_KEY}
 EOF
 
+cat <<EOF > /etc/traefik/dynamic/services.yml
+http:
+  services:
+    google-service:
+      loadBalancer:
+        servers:
+          - url: ${USER_SERVICE_URL}/api/v1/google
+
+    auth-service:
+      loadBalancer:
+        servers:
+          - url: ${USER_SERVICE_URL}/api/v1/auth
+
+    users-service:
+      loadBalancer:
+        servers:
+          - url: ${USER_SERVICE_URL}/api/v1/users
+
+    businesses-service:
+      loadBalancer:
+        servers:
+          - url: ${BUSINESS_SERVICE_URL}/api/v1/businesses
+
+    padel-courts-service:
+      loadBalancer:
+        servers:
+          - url: ${BUSINESS_SERVICE_URL}/api/v1/padel-courts
+
+  routers:
+    google-router:
+      rule: "PathPrefix(`/api/v1/google`)"
+      service: google-service
+      entryPoints:
+        - websecure
+      tls:
+        certResolver: duckdns
+      middlewares:
+        - cors
+        - my-easy-traefik-rate-limit-jwt
+        - x-api-key
+
+    auth-router:
+      rule: "PathPrefix(`/api/v1/auth`)"
+      service: auth-service
+      entryPoints:
+        - websecure
+      tls:
+        certResolver: duckdns
+      middlewares:
+        - cors
+        - my-easy-traefik-rate-limit-jwt
+        - x-api-key
+
+    users-router:
+      rule: "PathPrefix(`/api/v1/users`)"
+      service: users-service
+      entryPoints:
+        - websecure
+      tls:
+        certResolver: duckdns
+      middlewares:
+        - cors
+        - my-easy-traefik-rate-limit-jwt
+        - x-api-key
+
+    businesses-router:
+      rule: "PathPrefix(`/api/v1/businesses`)"
+      service: businesses-service
+      entryPoints:
+        - websecure
+      tls:
+        certResolver: duckdns
+      middlewares:
+        - cors
+        - my-easy-traefik-rate-limit-jwt
+        - x-api-key
+
+    padel-courts-router:
+      rule: "PathPrefix(`/api/v1/padel-courts`)"
+      service: padel-courts-service
+      entryPoints:
+        - websecure
+      tls:
+        certResolver: duckdns
+      middlewares:
+        - cors
+        - my-easy-traefik-rate-limit-jwt
+        - x-api-key
+
+EOF
+
 exec traefik "$@"
