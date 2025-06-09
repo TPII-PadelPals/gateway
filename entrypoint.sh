95554ec32b18956ba8adc404208c7aafeb31b122
tdelpup <tomasgdelpup@gmail.com>
2025-06-09 00:14:06 +0000

    fix: config

--- entrypoint.sh
+++ entrypoint.sh
@@ -84,7 +84,7 @@ http:
 
   routers:
     google-router:
-      rule: "PathPrefix(\`/api/v1/google\`)"
+      rule: "Host(\`padelpals.duckdns.org\`) && PathPrefix(\`/api/v1/google\`)"
       service: google-service
       entryPoints:
         - websecure
@@ -96,7 +96,7 @@ http:
         - x-api-key
 
     auth-router:
-      rule: "PathPrefix(\`/api/v1/auth\`)"
+      rule: "Host(\`padelpals.duckdns.org\`) && PathPrefix(\`/api/v1/auth\`)"
       service: auth-service
       entryPoints:
         - websecure
@@ -108,7 +108,7 @@ http:
         - x-api-key
 
     users-router:
-      rule: "PathPrefix(\`/api/v1/users\`)"
+      rule: "Host(\`padelpals.duckdns.org\`) && PathPrefix(\`/api/v1/users\`)"
       service: users-service
       entryPoints:
         - websecure
@@ -120,7 +120,7 @@ http:
         - x-api-key
 
     businesses-router:
-      rule: "PathPrefix(\`/api/v1/businesses\`)"
+      rule: "Host(\`padelpals.duckdns.org\`) && PathPrefix(\`/api/v1/businesses\`)"
       service: businesses-service
       entryPoints:
         - websecure
@@ -132,7 +132,7 @@ http:
         - x-api-key
 
     padel-courts-router:
-      rule: "PathPrefix(\`/api/v1/padel-courts\`)"
+      rule: "Host(\`padelpals.duckdns.org\`) && PathPrefix(\`/api/v1/padel-courts\`)"
       service: padel-courts-service
       entryPoints:
         - websecure
@@ -144,7 +144,7 @@ http:
         - x-api-key
 
     matches-router:
-      rule: "PathPrefix(\`/api/v1/matches\`)"
+      rule: "Host(\`padelpals.duckdns.org\`) && PathPrefix(\`/api/v1/matches\`)"
       service: matches-service
       entryPoints:
         - websecure
